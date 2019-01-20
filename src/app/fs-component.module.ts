f665116383dc468fe08a7c04bef90fe6f0f31ab2
Raymond Gigliotti <ray@firestitch.com>
2019-01-20 11:32:13 -0500

    initial commit

--- src/app/fs-component.module.ts
+++ src/app/fs-component.module.ts
@@ -2,11 +2,19 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FsComponentComponent } from './components/fs-component/fs-component.component';
+
 // import { FsComponentService } from './services';
+import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule, MatTreeModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatTreeModule
   ],
   exports: [
     FsComponentComponent,
---
+++ src/app/models/helpers.ts
@@ -0,0 +1,35 @@
+/**
+ * Node for to-do item
+ */
+export class TodoItemNode {
+  children: TodoItemNode[];
+  item: string;
+}
+
+/** Flat to-do item node with expandable and level information */
+export class TodoItemFlatNode {
+  item: string;
+  level: number;
+  expandable: boolean;
+}
+
+/**
+ * The Json object for to-do list data.
+ */
+export const TREE_DATA = {
+  Groceries: {
+    'Almond Meal flour': null,
+    'Organic eggs': null,
+    'Protein Powder': null,
+    Fruits: {
+      Apple: null,
+      Berries: ['Blueberry', 'Raspberry'],
+      Orange: null
+    }
+  },
+  Reminders: [
+    'Cook dinner',
+    'Read the Material Design spec',
+    'Upgrade Application to Angular'
+  ]
+};
