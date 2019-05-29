ceda08d11b67f6b837230ee55fe09805589d6c68
Raymond Gigliotti <ray@firestitch.com>
2019-05-29 12:45:53 -0400

    updated to new fsexample standard

--- playground/app/app.component.ts
+++ playground/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from '@env';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+	public config = environment;
 }
